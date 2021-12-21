b30954e4dc80988448c0024a19241a97e89cd828
2021dfff <1374490218@qq.com>
2021-12-21 22:04:37 +0800

    12.21 ui (without definition)

--- load.py
+++ load.py
@@ -12,29 +12,6 @@ class Main_window:
         self.ui = QUiLoader().load('main_window.ui')
         #self.ui.button.clicked.connect(self.handleCalc)
 
-    def handleCalc(self):
-        info = self.ui.textEdit.toPlainText()
-
-        salary_above_20k = ''
-        salary_below_20k = ''
-        for line in info.splitlines():
-            if not line.strip():
-                continue
-            parts = line.split(' ')
-
-            parts = [p for p in parts if p]
-            name,salary,age = parts
-            if int(salary) >= 20000:
-                salary_above_20k += name + '\n'
-            else:
-                salary_below_20k += name + '\n'
-
-        QMessageBox.about(self.ui,
-                    '统计结果',
-                    f'''薪资20000 以上的有：\n{salary_above_20k}
-                    \n薪资20000 以下的有：\n{salary_below_20k}'''
-                    )
-
 app = QApplication([])
 main_window = Main_window()
 main_window.ui.show()
